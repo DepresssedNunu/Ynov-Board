79ea560d604bf0959051889f962fc3dbe01f4bcf
aflorian <florian.albora@ynov.com>
2023-11-21 09:46:47 +0100

    factory

--- Ynov.Data/Contexts/IDesignTimeDbContextFactory.cs
+++ Ynov.Data/Contexts/IDesignTimeDbContextFactory.cs
@@ -1,24 +1,24 @@
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.Design;
-// using Microsoft.Extensions.Configuration;
-//
-//
-// namespace Ynov.Data.Contexts
-// {
-//     public class BoardDbContextFactory : IDesignTimeDbContextFactory<BoardDbContext>
-//     {
-//         public BoardDbContext CreateDbContext(string[] args)
-//         {
-//             IConfigurationRoot configuration = new Configuration()
-//                 .SetBasePath(Directory.GetCurrentDirectory())
-//                 .AddJsonFile("./Ynov/Ynov.API/appsettings.json")
-//                 .Build();
-//
-//             var builder = new DbContextOptionsBuilder<BoardDbContext>();
-//             var connectionString = configuration.GetConnectionString("DefaultConnection");
-//             builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-//
-//             return new BoardDbContext(builder.Options);
-//         }
-//     }
-// }
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Ynov.Data.Contexts
+{
+    public class BoardDbContextFactory : IDesignTimeDbContextFactory<BoardDbContext>
+    {
+        public BoardDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<BoardDbContext>();
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            return new BoardDbContext(builder.Options);
+        }
+    }
+}
