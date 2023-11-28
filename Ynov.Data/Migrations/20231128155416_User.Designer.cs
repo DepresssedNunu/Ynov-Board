58b075d4dd0a323b83c2c1e2ba32b588f29f0724
aflorian <florian.albora@ynov.com>
2023-11-28 16:54:54 +0100

    add user migration

--- Ynov.Data/Migrations/20231128155416_User.Designer.cs
+++ Ynov.Data/Migrations/20231128155416_User.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ynov.Data.Contexts;
@@ -12,9 +13,11 @@ using Ynov.Data.Contexts;
 namespace Ynov.Data.Migrations
 {
     [DbContext(typeof(BoardDbContext))]
-    partial class BoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231128155416_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
