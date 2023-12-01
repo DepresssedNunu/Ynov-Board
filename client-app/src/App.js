ff3abbe5849575558231dc3d464bd213a5eace28
aflorian <florian.albora@ynov.com>
2023-12-01 10:19:23 +0100

    add babel

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import '@radix-ui/themes/styles.css';
+import Test from "../components/Test";
 
 function App() {
   return (
@@ -17,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <Test/>
+        
       </header>
     </div>
   );
